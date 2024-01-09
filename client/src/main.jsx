f88e530c00658f72b8353a2376cd0a75bbcb6b89
Adam <105974022+elimelechAdam@users.noreply.github.com>
2024-01-09 19:00:26 +0200

    updated disabledevtools

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,11 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
