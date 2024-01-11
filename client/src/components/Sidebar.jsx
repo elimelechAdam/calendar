c5dcd2b25193f3cb5119cd26458585d0ca9e272e
Adam <105974022+elimelechAdam@users.noreply.github.com>
2024-01-11 12:28:00 +0200

    sdiebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -104,12 +104,6 @@ const Sidebar = ({ name }) => {
             </ListItem>
           </List>
         </div>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={getUsersDetails}
-        >
-          Test all users
-        </button>
         <div className="">
           <SignOutButton />
           <CopyRight />
