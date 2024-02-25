3ce92265c4cc05130747149810334dd1112bf6aa
Adam <105974022+elimelechAdam@users.noreply.github.com>
2024-02-25 17:44:33 +0200

    updated Permissions.js

--- api/src/controllers/permissions.js
+++ api/src/controllers/permissions.js
@@ -41,26 +41,6 @@ route.get("/:email", async (req, res) => {
   }
 });
 
-// route.put("/:id", async (req, res) => {
-//   const { id } = req.params;
-//   const { requestStatus } = req.body;
-//   try {
-//     const updatedRequest = await Request.findByIdAndUpdate(
-//       id,
-//       { requestStatus },
-//       { new: true }
-//     );
-
-//     if (!updatedRequest) {
-//       return res.status(404).json({ message: "No request found" });
-//     }
-
-//     res.status(200).json(updatedRequest);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// });
-
 // PUT route to update a request based on the provided ID and the fields in the request body
 route.put("/:id", async (req, res) => {
   const { id } = req.params;
